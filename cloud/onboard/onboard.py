6a38cd10cfbbcecf8918b878c1dc2f004be38340
Jason Mobarak <jam@cozybit.com>
2014-01-24 16:27:17 -0800

    Fixup website style and use jquery for status

--- cloud/onboard/onboard.py
+++ cloud/onboard/onboard.py
@@ -16,82 +16,84 @@ import webapp2
 import jinja2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-	extensions=['jinja2.ext.autoescape'],
-	autoescape=True)
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 def fuzzy_readable_time(delta):
-	if (delta.total_seconds() < 1):
-		return "just now"
-	if (delta.total_seconds() < 2):
-		return str(delta.seconds) + " second ago"
-	if (delta.total_seconds() < 60):
-		return str(delta.seconds) + " seconds ago"
-	elif delta.total_seconds() < 120:
-		return str(delta.seconds / 60) + " minute ago"
-	elif delta.total_seconds() < 3600:
-		return str(delta.seconds / 60) + " minutes ago"
-	elif delta.total_seconds() < 2 * 3600:
-		return str(delta.seconds / 60) + " hour ago"
-	elif delta.total_seconds() < 24 * 3600:
-		return str(delta.seconds / 3600) + " hours ago"
-	elif delta.total_seconds() < 2 * 24 * 3600:
-		return str(delta.seconds / 3600) + " day ago"
-	else:
-		return str(delta.days) + " days ago"
+    if not delta:
+        return "never"
+    if (delta.total_seconds() < 1):
+        return "just now"
+    if (delta.total_seconds() < 2):
+        return str(delta.seconds) + " second ago"
+    if (delta.total_seconds() < 60):
+        return str(delta.seconds) + " seconds ago"
+    elif delta.total_seconds() < 120:
+        return str(delta.seconds / 60) + " minute ago"
+    elif delta.total_seconds() < 3600:
+        return str(delta.seconds / 60) + " minutes ago"
+    elif delta.total_seconds() < 2 * 3600:
+        return str(delta.seconds / 60) + " hour ago"
+    elif delta.total_seconds() < 24 * 3600:
+        return str(delta.seconds / 3600) + " hours ago"
+    elif delta.total_seconds() < 2 * 24 * 3600:
+        return str(delta.seconds / 3600) + " day ago"
+    else:
+        return str(delta.days) + " days ago"
 
 def validate_did(did):
-		"""A did has the format of a mac address XX:XX:XX:XX:XX:XX"""	
-		return re.match(r'([0-9A-F]{2}[:-]){5}([0-9A-F]{2})', did, re.I) != None
+        """A did has the format of a mac address XX:XX:XX:XX:XX:XX"""
+        return re.match(r'([0-9A-F]{2}[:-]){5}([0-9A-F]{2})', did, re.I) != None
 
 def validate_status(status):
-		return status == "on" or status == "off"
+        return status == "on" or status == "off"
 
 class DeviceNode(ndb.Model):
-	"""Models onboardee device"""
-	last_seen = ndb.DateTimeProperty(auto_now=True)
-	status = ndb.StringProperty();
+    """Models onboardee device"""
+    last_seen = ndb.DateTimeProperty(auto_now=True)
+    status = ndb.StringProperty();
 
 class CheckIn(webapp2.RequestHandler):
-	def get(self):
-		# Maybe some tiny embedded nodes don't support post so accept
-		# GET checkins as well.
-		self.post()
+    def get(self):
+        # Maybe some tiny embedded nodes don't support post so accept
+        # GET checkins as well.
+        self.post()
 
-	def post(self):
-		vendorid = self.request.str_params['vendorid']
-		logging.info('vendorid: ' + vendorid)
-		try: 
-			namespace_manager.set_namespace(vendorid)
-		except:
-			self.error(400);
-			return
+    def post(self):
+        vendorid = self.request.str_params['vendorid']
+        logging.info('vendorid: ' + vendorid)
+        try:
+            namespace_manager.set_namespace(vendorid)
+        except:
+            self.error(400);
+            return
 
-		deviceid = self.request.str_params['deviceid'].lower()
-		logging.info('deviceid: ' + deviceid)
-		if not validate_did(deviceid):
-			self.error(400)
-			return
+        deviceid = self.request.str_params['deviceid'].lower()
+        logging.info('deviceid: ' + deviceid)
+        if not validate_did(deviceid):
+            self.error(400)
+            return
 
-		status = self.request.str_params['status'].lower()
-		logging.info('status: ' + status)
-		if not validate_status(status):
-			self.error(400)
-			return
+        status = self.request.str_params['status'].lower()
+        logging.info('status: ' + status)
+        if not validate_status(status):
+            self.error(400)
+            return
 
-		node = DeviceNode.get_by_id(id=deviceid)
-		if (node == None):
-			node = DeviceNode(id=deviceid)
-			logging.info('new DeviceNode: ' + deviceid)
-	
-		if node.status != status:
-			node.status = status
-			clientid = vendorid + deviceid
-			channel.send_message(clientid, status)
+        node = DeviceNode.get_by_id(id=deviceid)
+        if (node == None):
+            node = DeviceNode(id=deviceid)
+            logging.info('new DeviceNode: ' + deviceid)
 
-		node.put()
+        if node.status != status:
+            node.status = status
+            clientid = vendorid + deviceid
+            channel.send_message(clientid, status)
 
-		return
+        node.put()
+
+        return
 
 class MainPage(webapp2.RequestHandler):
 
@@ -101,49 +103,51 @@ class MainPage(webapp2.RequestHandler):
 
 class ChannelStatus(webapp2.RequestHandler):
     def post(self):
-		clientid = self.request.get('from')
-		action = self.request.path.split('/')[-2]
-		logging.info('clientid ' + clientid + ' is now ' + action)
+        clientid = self.request.get('from')
+        action = self.request.path.split('/')[-2]
+        logging.info('clientid ' + clientid + ' is now ' + action)
 
 class StatusPage(webapp2.RequestHandler):
-	def get(self, vendorid, deviceid):
-		try:
-			namespace_manager.set_namespace(vendorid)
-		except:
-			self.error(400)
-			return
+    def get(self, vendorid, deviceid):
+        try:
+            namespace_manager.set_namespace(vendorid)
+        except:
+            self.error(400)
+            return
 
-		if not validate_did(deviceid):
-			self.error(400)
-			return
+        if not validate_did(deviceid):
+            self.error(400)
+            return
 
-		node = DeviceNode.get_by_id(deviceid)
-		last_update = datetime.datetime.now() - node.last_seen
-		logging.info('last_update: ' + str(last_update))
+        node = DeviceNode.get_by_id(deviceid)
+        last_update = None
+        if node is not None:
+            last_update = datetime.datetime.now() - node.last_seen
+        logging.info('last_update: ' + str(last_update))
 
-		clientid = vendorid + deviceid
-		token = channel.create_channel(clientid)
-		# logging.info('token ' + token)
+        clientid = vendorid + deviceid
+        token = channel.create_channel(clientid)
+        # logging.info('token ' + token)
 
-		template_values = {
-			'token': token,
-			'vendorid': vendorid,
-			'deviceid': deviceid,
-			'node': node,
-			'last_update': fuzzy_readable_time(last_update),
-		}
+        template_values = {
+            'token': token,
+            'vendorid': vendorid,
+            'deviceid': deviceid,
+            'node': node,
+            'last_update': fuzzy_readable_time(last_update),
+        }
 
-		template = JINJA_ENVIRONMENT.get_template('index.html')
-		self.response.write(template.render(template_values))
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 
-		return
+        return
 
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-	('/checkin', CheckIn),
-	('/status/([-\w]+)/([-:\w]+)', StatusPage),
-	('/_ah/channel/connected/', ChannelStatus),
-	('/_ah/channel/disconnected/', ChannelStatus),
+    ('/checkin', CheckIn),
+    ('/status/([-\w]+)/([-:\w]+)', StatusPage),
+    ('/_ah/channel/connected/', ChannelStatus),
+    ('/_ah/channel/disconnected/', ChannelStatus),
 ], debug=True)
 
