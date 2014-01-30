61135d59d5a97c8b63c396a4c2d71e47df3eefa4
Jason Mobarak <jam@cozybit.com>
2014-01-29 17:26:20 -0800

    Fix launch/startup script to be less static

--- peripheral/launch.sh
+++ peripheral/launch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+D=$(dirname $(readlink -f ${BASH_SOURCE}))
+
+die () { echo ${*}; exit -1; }
 
 ONBOARD_NODE_MODULES=$1
-ONBOARD_WLAN_DEVICE=$2
-ONBOARD_DEVICE_ID=$3
-ONBOARD_VENDOR_ID=$4
+[[ -n $ONBOARD_NODE_MODULES ]] || die "Usage: launch.sh <node_modules>"
+
+ONBOARD_WLAN_DEVICE=$(iw dev | grep Interface | awk '{print $2}')
+ONBOARD_DEVICE_ID=$(hciconfig | grep "BD Address" | head -1 | awk '{print $3}')
+ONBOARD_VENDOR_ID="toastmaster"
+
+[[ ${#ONBOARD_DEVICE_ID} -eq 17 ]] \
+    || die "Device ID was invalid: $ONBOARD_DEVICE_ID"
 
 exec sudo \
 	ONBOARD_NODE_MODULES=$ONBOARD_NODE_MODULES \
--- peripheral/startup.sh
+++ peripheral/startup.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-die () { echo ${*}; exit -1; }
+D=$(dirname $(readlink -f ${BASH_SOURCE}))
 
-wlan_dev=$(iw dev|grep Interface|awk '{print $2}')
 node_modules=/home/linaro/ble/node_modules
-device_id=$(hciconfig | grep "BD Address" | head -1 | awk '{print $3}')
-vendor_id="toastmaster"
 
-[[ ${#device_id} -eq 17 ]] || die "Device ID was invalid: $device_id"
-
-sudo su <<EOF
-	~/onboarding_demo/peripheral/gpio.sh
-	~/onboarding_demo/peripheral/monitor_oven.sh&
-	~/onboarding_demo/peripheral/launch.sh \
-        "$node_modules" "$wlan_dev" "$device_id" "$vendor_id"
-EOF
+sudo su $D/gpio.sh
+sudo $D/monitor_oven.sh &
+sudo $D/launch.sh "$node_modules"
