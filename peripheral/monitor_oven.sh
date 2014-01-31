3e48546c7e145cc9b3c2e5fcbf92b8ad3bcb5546
Jason Mobarak <jam@cozybit.com>
2014-01-31 12:18:18 -0800

    Make the monitor script work at first boot
    At first boot when we don't have connectivity, make sure that the script
    will retry posting it's status for as long as it takes to check in
    successfully.

--- peripheral/monitor_oven.sh
+++ peripheral/monitor_oven.sh
@@ -4,20 +4,35 @@ D=$(dirname $(readlink -f ${BASH_SOURCE}))
 source $D/library.sh
 
 HOST=cozyonboard.appspot.com
-#HOST=192.168.1.104:8080
 
 DEV_ID=$1
 [[ -n $DEV_ID ]] || die "Usage: monitor_over.sh <devid>"
 
 STATE=-1
 
+checkin() {
+    while true; do
+        STATE=`cat /sys/class/gpio/gpio4_pc21/value`
+        [ ${STATE} -eq "0" ] && STATUS="off"
+        [ ${STATE} -eq "1" ] && STATUS="on"
+        wget \
+            --timeout=5 \
+            -O /dev/null \
+            "http://$HOST/checkin?vendorid=toastmaster&deviceid=${DEV_ID}&status=${STATUS}"
+        if [[ $? -eq 0 ]]; then
+            break;
+        fi
+        sleep 0.5
+    done
+}
+
 while true;
 do
-	sleep 0.1
-	PREV_STATE=${STATE}
-	STATE=`cat /sys/class/gpio/gpio4_pc21/value`
-	[ ${PREV_STATE} -eq ${STATE} ] && continue
-	[ ${STATE} -eq "0" ] && STATUS="off"
-	[ ${STATE} -eq "1" ] && STATUS="on"
-	wget -O /dev/null "http://$HOST/checkin?vendorid=toastmaster&deviceid=${DEV_ID}&status=${STATUS}"
+    sleep 0.1
+    PREV_STATE=${STATE}
+    STATE=`cat /sys/class/gpio/gpio4_pc21/value`
+    [ ${PREV_STATE} -eq ${STATE} ] && continue
+    checkin
 done
+
+# vim:et:sts:ts=4:sw=4:
