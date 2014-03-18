890e1bf8f202fa5eb99987942fa6201015bcf49a
Guillermo A Nunez <guillermo@cozybit.com>
2014-03-18 13:00:50 -0700

    [onboardee-service] Add initial version of the project.

---
+++ onbordee-service/src/com/cozybit/onbordee/OnboardeeActivity.java
@@ -0,0 +1,51 @@
+package com.cozybit.onbordee;
+
+import com.cozybit.onbordee.service.OnboardeeService;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class OnboardeeActivity extends Activity {
+
+	private Button mStartServiceButton;
+	private Button mStopServiceButton;
+	private Intent mServiceIntent;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_onboardee);
+		
+		mStartServiceButton = (Button) findViewById(R.id.startServiceButton);
+		mStopServiceButton = (Button) findViewById(R.id.stopServiceButton);
+		
+		mServiceIntent = new Intent(this, OnboardeeService.class);
+		
+		mStartServiceButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startService(mServiceIntent);
+			}
+		});
+
+		mStopServiceButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stopService(mServiceIntent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.onboardee, menu);
+		return true;
+	}
+
+}
