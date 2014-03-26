90eac40f58ea3f1accf1bfac32f56e4ee321f53e
Guillermo A Nunez <guillermo@cozybit.com>
2014-03-26 16:17:20 -0700

    [onboarding apps] Change the UUID for the onboarding service.
    Android has issues publishing unknown/non-registered services. As a
    workaround, use the HEART RATE uuid for now.

--- WandOnboarding/src/com/cozybit/onboarding/ble/OnboardingGattService.java
+++ WandOnboarding/src/com/cozybit/onboarding/ble/OnboardingGattService.java
@@ -5,7 +5,13 @@ import java.util.UUID;
 public class OnboardingGattService {
 	
 	/* Service UUID */
-	public static final UUID SERVICE_UUID = UUID.fromString("FFFFFFFF-C0C1-FFFF-C0C1-201401000000");
+	/*FIXME: for some reason, android devices can't discover our custom service with this UUID. 
+	 *  don'tknow if the reason is that the UUID is malformed and it's using some bits that it's
+	 *  not supposed to, or that the Android device is looking for a range of services, etc. 
+	 *  It's a mystery. For now, use the HEART RATE PROFILE UUID as a workaround.
+	 */
+	//public static final UUID SERVICE_UUID = UUID.fromString("FFFFFFFF-C0C1-FFFF-C0C1-201401000000");
+	public static final UUID SERVICE_UUID = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb");
 
 	/* Characteristics UUIDs */
 	public static final UUID CHARACTERISTIC_STATUS = UUID.fromString("FFFFFFFF-C0C1-FFFF-C0C1-201401000001");
