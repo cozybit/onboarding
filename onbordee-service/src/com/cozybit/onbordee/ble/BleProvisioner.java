586060bf5789f51fd7fe8a72278e2e6031018562
Guillermo A Nunez <guillermo@cozybit.com>
2014-04-04 17:44:18 -0700

    [onboardee-service] Add OnboardingProfile class and refactor BleProvisioner

--- onbordee-service/src/com/cozybit/onbordee/ble/BleProvisioner.java
+++ onbordee-service/src/com/cozybit/onbordee/ble/BleProvisioner.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import com.cozybit.onbordee.manager.ConnectionManager.DataReceivedCallback.DataTypes;
 import com.cozybit.onbordee.manager.ConnectionManager.Events;
-import com.cozybit.onbordee.manager.ConnectionManager.SubEvents;
 import com.cozybit.onbordee.manager.IManager;
+import com.cozybit.onbordee.profile.OnboardingProfile;
 
+import com.cozybit.onbordee.utils.BluetoothConstants.BT_PROFILE_STATE;
+import com.cozybit.onbordee.utils.BluetoothConstants.GATT_STATUS;
 import com.cozybit.onbordee.utils.Log;
 
 import android.bluetooth.BluetoothAdapter;
@@ -26,25 +27,24 @@ import android.os.Message;
 
 public class BleProvisioner {
 
-	private String TAG = BleProvisioner.class.getName();
-	
+	private final static String TAG = BleProvisioner.class.getName();
+
+	public static int GATT_CLIENT = 1;
+	public static int GATT_SERVER = 2;
+
 	private Context mContext;
 	private IManager mManager;
-	
+
 	private boolean mBtInitState;
-	private BluetoothDevice mBtClientDev;
 	private BluetoothManager mBluetoothManager;
 	private BluetoothAdapter mBluetoothAdapter;
-	
-	private UUID mServiceUUID = OnboardingGattService.SERVICE_UUID;
-	
-    //private BluetoothGatt mBluetoothGatt;
+
     private BluetoothGattServer mGattServer;
     private BluetoothGattService mGattService;
     private BleBlockingQueue mBleBlockingQueue;
-    
+
 	private BluetoothGattServerCallback mBtGattServerCbk = new BluetoothGattServerCallback() {
-    	
+    
     	/* Missing methods to implement. Keep them in mind, because we might need them for something:
     	 *   
     	 *  void 	onDescriptorReadRequest(BluetoothDevice device, int requestId,
@@ -61,33 +61,13 @@ public class BleProvisioner {
     	@Override
     	public void onCharacteristicReadRequest(BluetoothDevice device, int requestId,
     			 int offset, BluetoothGattCharacteristic characteristic) {
-    		 Log.d(TAG, "onCharacteristicReadRequest -> uuid: %s", characteristic.getUuid().toString());
+    		 
+    		 OnboardingProfile.Characteristics type = OnboardingProfile.Characteristics.valueOf( characteristic.getUuid() );
+    		 Log.d(TAG, "Type: %s; Value: %s", type, Arrays.toString( characteristic.getValue() ) );
     		 
     		 if (mGattServer != null) {
-    			 
-    			byte[] value = new byte[1];
-    			UUID uuid = characteristic.getUuid();
-
-    			if( uuid.equals(OnboardingGattService.CHARACTERISTIC_STATUS) ) {
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_LONG_STATUS) ) {
-    				
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_SSID) ) {
-    				
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_AUTH) ) {
-    				
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_PASS) ) {
-    				
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_CHANNEL) ) {
-    				
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_VENDOR_ID) ) {
-    				value[0] = 100;
-    			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_DEVICE_ID) ) {
-    				value[0] = 101;
-    			} else {
-    				Log.d(TAG, "Unknown characteristic! --> uuid: %s", uuid.toString() );
-    			}
-    			
-    			mGattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, value);
+    			boolean ret = mGattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, characteristic.getValue());
+    			if(!ret) Log.e(TAG, "ERROR: sending a ReadRequest response for %s to %s", type, device.getAddress());
     		 }
     	}
     	
@@ -96,27 +76,14 @@ public class BleProvisioner {
     	public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, 
     			BluetoothGattCharacteristic characteristic, boolean preparedWrite, 
     			boolean responseNeeded, int offset, byte[] value) {
-    		Log.d(TAG, "UUID: %s, Value: %s", characteristic.getUuid(), Arrays.toString(value) );
+    		
+    		OnboardingProfile.Characteristics type = OnboardingProfile.Characteristics.valueOf( characteristic.getUuid() );
+    		Log.d(TAG, "Type: %s; Value: %s", type, Arrays.toString(value) );
 
     		Message msg = Message.obtain();
     		msg.what = Events.RECEIVED_DATA.ordinal();
+    		msg.arg1 = type.ordinal();
     		msg.obj = value;
-    		
-   			UUID uuid = characteristic.getUuid();
-		
-    		if( uuid.equals(OnboardingGattService.CHARACTERISTIC_SSID) ) {
-    			msg.arg1 = DataTypes.SSID.ordinal();
-    		} else if ( uuid.equals(OnboardingGattService.CHARACTERISTIC_AUTH) ) {
-    			msg.arg1 = DataTypes.AUTH.ordinal();
-    		} else if ( uuid.equals(OnboardingGattService.CHARACTERISTIC_PASS) ) { 
-    			msg.arg1 = DataTypes.PASS.ordinal();
-    		} else if ( uuid.equals(OnboardingGattService.CHARACTERISTIC_CHANNEL) ) {
-    			msg.arg1 = DataTypes.CHANNEL.ordinal();
-			} else if( uuid.equals(OnboardingGattService.CHARACTERISTIC_COMMAND) ) {
-				msg.arg1 = DataTypes.CMD.ordinal();
-			} else
-				Log.d(TAG, "Unknown characteristic! --> uuid: " + uuid.toString() );
-    		
     		mManager.sendMessage(msg);
     		mGattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, value);
     	}
@@ -124,10 +91,12 @@ public class BleProvisioner {
 		//Callback indicating when a remote device has been connected or disconnected.
 		@Override
 		public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
-			Log.d(TAG, "Device: %s | Status: %d | newSate: %d", device.getAddress(), status, newState);
-			
+			GATT_STATUS gStatus = GATT_STATUS.valueOf(status);
+			BT_PROFILE_STATE pState = BT_PROFILE_STATE.valueOf(newState);
+			Log.d(TAG, "Device: %s | Status: %s | newSate: %s", device.getAddress(), gStatus, pState);
+
 			Message msg = Message.obtain();
-			
+
 			switch(newState) {
 			case BluetoothProfile.STATE_CONNECTED:
 				msg.what = Events.CLIENT_CONNECTED.ordinal();
@@ -140,7 +109,7 @@ public class BleProvisioner {
 			default:
 				break;
 			}
-			
+
 			mManager.sendMessage(msg);
 		}
     	
@@ -153,8 +122,12 @@ public class BleProvisioner {
     	//Indicates whether a local service has been added successfully.
     	@Override
     	public void onServiceAdded(int status, BluetoothGattService service) {
-   		 	Log.d(TAG, "Status: %d Service (UUID): %s", status, service.getUuid() );
-   		 	mManager.sendMessage(Events.GATT_SERVER_DEPLOYED);
+    		GATT_STATUS gStatus = GATT_STATUS.valueOf(status);
+   		 	Log.d(TAG, "Status: %s; Service (UUID): %s", gStatus, service.getUuid() );
+   		 	if( gStatus == GATT_STATUS.SUCCESS )
+   		 		mManager.sendMessage(Events.GATT_SERVER_DEPLOYED);
+   		 	else
+   		 		mManager.sendMessage(Events.GATT_SERVER_FAILED);
     	}
     	
 	};
@@ -170,7 +143,7 @@ public class BleProvisioner {
 		
 		if (mContext == null) {
 			Log.e(TAG,"ERROR: context not available.");
-			mManager.sendMessage(Events.INIT_ERROR);
+			//mManager.sendMessage(Events.INIT_ERROR);
 			return;
 		}
 		
@@ -180,22 +153,22 @@ public class BleProvisioner {
         // check for Bluetooth support
         if ( mBluetoothAdapter == null) {
         	Log.e(TAG, "ERROR: this android device has no Bluetooth support.");
-        	mManager.sendMessage(Events.INIT_ERROR, SubEvents.NO_BLE_AVAILABLE);
+        	mManager.sendMessage(Events.NO_BT);
         	return;
         } /*else if ( mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE) ) {
         	// N5, MK908 seems to not have BLE enabled as featurecheck for BLE support.
         	Log.e(TAG, "ERROR: this android device has no Bluetooth LE support.");
-        	mBtConMngr.sendMessage(Events.INIT_ERROR, SubEvents.NO_BLE_AVAILABLE);
+        	mBtConMngr.sendMessage(Events.NO_BLE);
         	return;
         }*/ else {
             if( mBtInitState = mBluetoothAdapter.isEnabled() ) {
             	mManager.sendMessage(Events.INIT);
-            	mManager.sendMessage(Events.BLUETOOTH_ON);
+            	mManager.sendMessage(Events.BT_ON);
             } else {
             	if( mBluetoothAdapter.enable() )
             		mManager.sendMessage(Events.INIT);
             	else
-        			mManager.sendMessage(Events.INIT_ERROR, SubEvents.BT_BROKEN);
+        			mManager.sendMessage(Events.BT_BROKEN);
             }
         }
         
@@ -216,7 +189,7 @@ public class BleProvisioner {
 		
 		/*TODO: many times, BluetoothManager fails opening the GattServer 
 		 * and it returns null. Probably it's because we still have to wait for
-		 * things to get initialized */
+		 * things to get initialized. This seems to work. */
 		try { Thread.sleep(500); } catch (InterruptedException e) {}
 		
         mGattServer = mBluetoothManager.openGattServer(mContext, mBtGattServerCbk);
@@ -226,7 +199,7 @@ public class BleProvisioner {
         	return;
         }
 
-        mGattService = new BluetoothGattService(OnboardingGattService.SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY);        
+        mGattService = new BluetoothGattService(OnboardingProfile.SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY);        
         for (BluetoothGattCharacteristic c : generateCharacteristics() ) {
         	if( !mGattService.addCharacteristic(c) ) {
         		Log.e(TAG, "ERROR: characteristic (%s) couln't be added to the service.", c.getUuid());
@@ -255,157 +228,88 @@ public class BleProvisioner {
 	}
 
 	private BluetoothGattCharacteristic[] generateCharacteristics() {
-		
-        /*BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(characteristicUUID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
-        characteristic.setValue(77, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-        BluetoothGattDescriptor descriptor = new BluetoothGattDescriptor(descriptorUUID, BluetoothGattDescriptor.PERMISSION_READ);
-        characteristic.addDescriptor(descriptor);*/
 
 		/*TODO Jlo
 		 * - Different between property & permission?
 		 * - How to set a characteristic READ & WRITE
 		 * - Do we need descriptor?
 		*/ 
-		BluetoothGattCharacteristic[] characs = new BluetoothGattCharacteristic[9];
-		characs[0] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_STATUS, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
-		characs[0].setValue(1, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[1] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_LONG_STATUS, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
-		characs[1].setValue(2, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[2] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_SSID, BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE,
-				BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
-		characs[2].setValue(3, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[3] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_AUTH, BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE,
-				BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
-		characs[3].setValue(4, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[4] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_PASS, BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE,
-				BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
-		characs[4].setValue(5, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[5] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_CHANNEL, BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE,
-				BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
-		characs[5].setValue(6, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[6] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_COMMAND, BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_WRITE);
-		characs[6].setValue(7, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[7] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_VENDOR_ID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
-		characs[7].setValue(8, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
-		
-		characs[8] = new BluetoothGattCharacteristic(OnboardingGattService.CHARACTERISTIC_DEVICE_ID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
-		characs[8].setValue(9, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+		int count = OnboardingProfile.Characteristics.values().length;
+		BluetoothGattCharacteristic[] characs = new BluetoothGattCharacteristic[count];
+		for(int i=0; i < count; i++) {
+			OnboardingProfile.Characteristics c = OnboardingProfile.Characteristics.values()[i];
+			characs[i] = new BluetoothGattCharacteristic(c.uuid, c.properties , c.permissions );
+			characs[i].setValue(0, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+		}
 		
 		return characs;
 	}
-	
-	public void updateCharacteristic(final UUID characteristicUUID, final String value) {
+
+	public boolean updateCharacteristic(UUID uuid, byte value, boolean notify) {
+		return updateCharacteristic( uuid, new byte[] { value }, notify );
+	}
+
+	public boolean updateCharacteristic(UUID uuid, byte[] value, boolean notify) {
 		
-		final BluetoothGattCharacteristic characteristic = mGattService.getCharacteristic(characteristicUUID);
-		characteristic.setValue(value);
-		List<BluetoothDevice> BtDevs = mBluetoothManager.getConnectedDevices(BluetoothProfile.GATT_SERVER);
-		if(BtDevs != null) {
-			for (BluetoothDevice bluetoothDevice : BtDevs)
-				mGattServer.notifyCharacteristicChanged(bluetoothDevice, characteristic, true);
-		}
-	}
-
-	public void writeCharacteristic(final UUID characteristicUUID, final String value) {
-
-		final BluetoothGattCharacteristic characteristic = mGattService.getCharacteristic(characteristicUUID);
-
-  		if (characteristic == null) {
-  			Log.w(TAG, "Problem getting characteristic from UUID %s", characteristicUUID.toString());
-  			return;
-  		}
-  		
-  		mBleBlockingQueue.newRequest(characteristic, new Runnable() {
-  			@Override
-  			public void run() {
-  				
-  				characteristic.setValue(value);
-  				mGattService.addCharacteristic(characteristic);
-  				//TODO If this is not enough...try readding the characteristic to the service! 
-  				
-  		        //if (!mBluetoothGatt.writeCharacteristic(characteristic))
-  				//	Log.w(TAG, "writeCharacteristic not possible for UUID %s", characteristicUUID.toString());
-  			}
-  		}, true);
-        
-	}
-	
-	public void writeCharacteristic(final UUID characteristicUUID, final byte[] value) {
-        
-		final BluetoothGattCharacteristic characteristic =  mGattService.getCharacteristic(characteristicUUID);
-  		
-  		if (characteristic == null) {
-  			Log.w(TAG, "Problem getting characteristic from UUID %s", characteristicUUID.toString());
-  			return;
-  		}
-  		
-  		mBleBlockingQueue.newRequest(characteristic, new Runnable() {
-  			@Override
-  			public void run() {
-  				
-  				characteristic.setValue(value);
-  				mGattService.addCharacteristic(characteristic);
-  		        /*if (!mBluetoothGatt.writeCharacteristic(characteristic))
-  					Log.w(TAG, "writeCharacteristic not possible for UUID %s", characteristicUUID.toString());*/
-  			}
-  		}, true);
-        
-	}
-	
-	/*public void readCharacteristic(final UUID characteristicUUID) {
-    	
-		final BluetoothGattCharacteristic characteristic = getCharacteristicFromUUID(characteristicUUID);
-  		
-  		if (characteristic == null) {
-  			Log.w(TAG, "Problem getting characteristic from UUID " + characteristicUUID.toString());
-  			return;
-  		}
-  		
-		mBleBlockingQueue.newRequest(characteristic, new Runnable() {
-			@Override
-			public void run() {
-		        if (!mBluetoothGatt.readCharacteristic(characteristic))
-					Log.w(TAG, "readCharacteristic not possible for UUID " + characteristicUUID.toString());
-			}
-		}, true);
-        
-        
-    }
-    
-	public void setCharacteristicNotification(final UUID characteristicUUID,
-			final boolean enabled) {
+		BluetoothGattCharacteristic characteristic = null;
+		boolean ret;
 		
-		final BluetoothGattCharacteristic characteristic = getCharacteristicFromUUID(characteristicUUID);
-			
-		mBleBlockingQueue.newRequest(characteristic, new Runnable() {
-			@Override
-			public void run() {
-				// Get Service
-				
-		        if (!mBluetoothGatt.setCharacteristicNotification(characteristic, enabled))
-					Log.w(TAG, "setCharacteristicNotification not possible for UUID " + characteristicUUID.toString());
-		        
-		        List <BluetoothGattDescriptor> descriptors = characteristic.getDescriptors();
-		        for (BluetoothGattDescriptor bluetoothGattDescriptor : descriptors) {
-		        	if (enabled) {
-		        		if (!bluetoothGattDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE))
-		        			Log.w(TAG, "setValue not possible for UUID " + characteristicUUID.toString());
-		        	} else {
-		        		if (!bluetoothGattDescriptor.setValue(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE))
-		        			Log.w(TAG, "setValue not possible for UUID " + characteristicUUID.toString());
-		        	}
-		    		if (!mBluetoothGatt.writeDescriptor(bluetoothGattDescriptor))
-		    			Log.w(TAG, "writeDescriptor not possible for UUID " + characteristicUUID.toString());
+		if( ret = (mGattService != null) ) {
+			characteristic = mGattService.getCharacteristic(uuid);
+			if( ret = (characteristic != null) ) {
+				ret = characteristic.setValue(value);
+				if(!ret) Log.e(TAG, "ERROR: charectristic's value couldn't be updated (uuid: %s)", uuid);
+			} else
+				Log.e(TAG, "ERROR: charectristic does not exist (uuid: %s)", uuid);
+		} else
+			Log.e(TAG, "ERROR: GattService is null!");
+		
+		if(ret && notify) 
+			ret = notifyCharacteristicChange(characteristic);
+		
+		return ret;
+	}
+
+	public boolean updateCharacteristic(UUID uuid, String value, boolean notify) {
+
+		BluetoothGattCharacteristic characteristic = null;
+		boolean ret;
+		
+		if( ret = (mGattService != null) ) {
+			characteristic = mGattService.getCharacteristic(uuid);
+			if( ret = (characteristic != null) ) {
+				ret = characteristic.setValue(value);
+				if(!ret) Log.e(TAG, "ERROR: charectristic's value couldn't be updated (uuid: %s)", uuid);
+			} else
+				Log.e(TAG, "ERROR: charectristic does not exist (uuid: %s)", uuid);
+		} else
+			Log.e(TAG, "ERROR: GattService is null!");
+
+		if(ret && notify) 
+			ret = notifyCharacteristicChange(characteristic);
+		
+		return ret;
+	}
+	
+	private boolean notifyCharacteristicChange(BluetoothGattCharacteristic characteristic) {
+		
+		boolean ret;
+		
+		if( ret = (mGattServer != null) ) {
+			List<BluetoothDevice> BtDevs = mBluetoothManager.getConnectedDevices(BluetoothProfile.GATT_SERVER);
+			if(BtDevs != null) {
+				for (BluetoothDevice dev : BtDevs) {
+					ret = mGattServer.notifyCharacteristicChanged(dev, characteristic, true); 
+					if( !ret ) {
+						Log.e(TAG, "ERROR: while notifying to device (%s) that charasteristic (%s) changed", dev.getAddress(), characteristic.getUuid() );
+						break;
+					}
 				}
-			}
-		}, false);
-	}*/
-
-
+			} else
+				Log.w(TAG, "WARNING: BluetoothDevices is NULL!!");
+		} else 
+			Log.e(TAG, "ERROR: mGattServer is NULL!!");
+		
+		return ret;
+	}
 }
---
+++ onbordee-service/src/com/cozybit/onbordee/profile/OnboardingProfile.java
@@ -0,0 +1,101 @@
+package com.cozybit.onbordee.profile;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+
+public class OnboardingProfile {
+
+	/* Service UUID */
+	/*FIXME: for some reason, android devices can't discover our custom service with this UUID. 
+	 *  don'tknow if the reason is that the UUID is malformed and it's using some bits that it's
+	 *  not supposed to, or that the Android device is looking for a range of services, etc. 
+	 *  It's a mystery. For now, use the HEART RATE PROFILE UUID as a workaround.
+	 */
+	//public static final UUID SERVICE_UUID = UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000000");
+	public static final UUID SERVICE_UUID = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb");
+
+	
+	private final static int PROP_R = BluetoothGattCharacteristic.PROPERTY_READ;
+	private final static int PROP_W = BluetoothGattCharacteristic.PROPERTY_WRITE;
+	private final static int PER_R = BluetoothGattCharacteristic.PERMISSION_READ;
+	private final static int PER_W = BluetoothGattCharacteristic.PERMISSION_WRITE;
+	
+	// Definition of the onboarding characteristics: UUIDs, properties and permissions
+	public static enum Characteristics {
+		STATUS( UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000001"), PROP_R, PER_R),
+		LONG_STATUS(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000002"), PROP_R, PER_R),
+		SSID(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000003"), PROP_R | PROP_W, PER_R | PER_W),
+		AUTH(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000004"), PROP_R | PROP_W, PER_R | PER_W),
+		PASS(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000005"), PROP_R | PROP_W, PER_R | PER_W),
+		CHANNEL(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000006"), PROP_R | PROP_W, PER_R | PER_W),
+		COMMAND(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000007"), PROP_W, PER_W),
+		VENDOR_ID(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000008"), PROP_R, PER_R),
+		DEVICE_ID(UUID.fromString("ffffffff-c0c1-ffff-c0c1-201401000009"), PROP_R, PER_R);
+
+		private final static Map<UUID, Characteristics> map = new HashMap<UUID, Characteristics>();
+	    static {
+	        for (Characteristics c : Characteristics.values())
+	            map.put(c.uuid, c);
+	    }
+	    
+		final public UUID uuid;
+		final public int properties;
+		final public int permissions;
+
+		private Characteristics(UUID uuid, int properties, int permissions) {
+			this.uuid = uuid;
+			this.properties = properties;
+			this.permissions = permissions;
+		}
+
+	    public static Characteristics valueOf(UUID uuid) {
+	    	return map.get(uuid);
+	    }
+	};
+
+	/*
+	 * OPTIONAL VALUES FOR THE DIFFERENT CHARACTERISTICS
+	 */
+
+	public static enum STATES {
+		OFF, BOOTING, READY, CONNECTING, CONNECTED, FAILED
+	};
+
+	public enum EVENTS {
+		//init & stop events
+		NONE(""), INIT("Initializing"),
+		SHUT_DOWN("Shutting Down"),
+		//Wifi related events
+		WIFI_ON("Wifi Iface Enabled"), WIFI_OFF("Wifi Iface Disabled"),
+		WIFI_FAILED("Wifi Iface failed"), NO_WIFI("Wifi Iface no available"),
+		//Parameter related event
+		PARAMETER_RECEIVED("Receiving Oboarding parameters"), INVALID_PARAMETERS("Invalid Parameters"),
+		//Onboarding actions
+		CONNECT_TO_WIFI("Connecting to Wifi"), FORGET_WIFI("Forget Wifi"), RESET("Reseting"),
+		//High Level events related with the wifi connection establishment
+		WIFI_CONNECTED("Connected to wifi network"), WIFI_DISCONNECTED("Disconnected from network"),
+		//Lo level events related with the wifi connection establishment
+		AUTHENTICATING("Authenticating"), ASSOCIATING("Associating"), HAND_SHAKING("Hand Shaking"),
+		GETTING_IPADDR("Getting IP address"), CONN_ESTAB("Connection completed"),
+		WIFI_LINK_DISCONNECTED("Wifi Link disconnected");
+		public final String msg;
+
+	    private EVENTS(String msg) {
+	        this.msg = msg;
+	    }
+	}
+	
+	
+	// Definition of the different COMMANDS methods
+	public static enum COMMANDS {
+		CONNECT, DISCONNECT, RESET
+	};
+
+	// Definition of the different AUTH methods
+	public static enum AUTH {
+		NONE, WPA_PSK, WPA_EAP, IEEE8021X;
+	}
+}
